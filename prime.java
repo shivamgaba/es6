42b9348ce7fa53b8ac0113a4f8e152603fdb2c74
shivamgaba <40313019+shivamgaba@users.noreply.github.com>
2018-06-16 19:06:44 +0530

    check whether no is prime or not.

---
+++ prime.java
@@ -0,0 +1,13 @@
+public class prime{
+public static void main(String args[]){
+  int num=50;
+  prime pm=new prime();
+  pm.check(num);
+      }
+  void check(int n){
+    if(n%2==0){
+    System.out.println("not prime");
+    }
+    else{System.out.println("prime no");}
+      }
+  }
